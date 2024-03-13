23b124ff5d03121eef4f25c404c8e7c50cc0fdd6
HeuristicHiker <connor.dev.johnston@gmail.com>
2024-03-12 23:35:53 -0400

    Having issues compiling the truffle contract. It gives a semi generic issue with opcodes which aren't familiar to me so I think it's better to circle back to basics rather than bang my head on the wall till it compiles

--- solidity/contracts/theMostAmazingContract.sol
+++ solidity/contracts/theMostAmazingContract.sol
@@ -1,24 +1,26 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19;
 
 contract TheMostAmazingContract {
-    // at some point I'd like to use wallets to see if that's even a good approach but not using yet
+    address public owner;
+
+    // Struct for future use
     struct Wallet {
         string owner;
         uint32 eth_balance;
     }
 
-    // get balances from owner
-    // review why we use int32
     mapping(string => uint32) public balances;
 
-    // Need to use events because ???
     event Transfer(string sender, string receiver, uint32 amount);
 
-    // Not how real transfers work... duh
-    // we say it's public since we want anyone to be able to call it
-    // Will want to implement owner contract to test and see if we can limit this function later
-    function deposit(string memory owner, uint32 amount) public {
-        balances[owner] += amount;
+    constructor() {
+        owner = msg.sender;
+        balances["Owner"] = 1000;
+    }
+
+    function deposit(string memory act, uint32 amount) public {
+        balances[act] += amount;
     }
 
     function transferBalance(
@@ -26,15 +28,12 @@ contract TheMostAmazingContract {
         string memory receiver,
         uint32 amount
     ) public {
-        // Do they have the money
         require(
             balances[sender] >= amount,
             "You can't send what you don't have... duh"
         );
-
         balances[sender] -= amount;
         balances[receiver] += amount;
-
         emit Transfer(sender, receiver, amount);
     }
 }
