ee86e72db1439d0cefaa1a71300d6863a3df5364
HeuristicHiker <connor.dev.johnston@gmail.com>
2024-03-24 21:39:50 -0400

    Minor changes learning looping and mutability within rust context

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world! from hello_cargo");
+    println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    loop {
+        println!("Please input your guess.");
+        
+        let mut guess = String::new();
+
+        const x: i32 = 5;
+
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        print!("{secret_number} You guessed: {}\n", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
